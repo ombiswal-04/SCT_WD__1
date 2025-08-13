adc63dca17d3ff70c237154e7b4f97cf657d1e00
Biswa <biswa@example.com>
2025-08-13 10:19:00 +0530

    Initial commit

---
+++ LP1/script.js
@@ -0,0 +1,5 @@
+const learnMoreBtn = document.getElementById("learnMoreBtn");
+
+learnMoreBtn.addEventListener("click", () => {
+  alert("Thanks for clicking! This is where more info would appear.");
+});
